f251cc0bb47221f4a868c7238ff49cef9c18ea30
thutasann <thutasann2002@gmail.com>
2025-07-17 14:29:14 +0800

    chore: folder structure change

--- javascript/concurrency/semaphore/fetch_users.example.js
+++ javascript/concurrency/semaphore/fetch_users.example.js
@@ -1,33 +1,6 @@
 // @ts-check
 
-class Semaphore {
-  constructor(maxConcurrent = 1) {
-    this.maxConcurrent = maxConcurrent
-    this.current = 0
-    this.queue = []
-  }
-
-  async acquire() {
-    if (this.current < this.maxConcurrent) {
-      this.current++
-      return Promise.resolve()
-    }
-
-    return new Promise((resolve) => {
-      this.queue.push(resolve)
-    })
-  }
-
-  release() {
-    this.current--
-
-    if (this.queue.length > 0 && this.current < this.maxConcurrent) {
-      this.current++
-      const next = this.queue.shift()
-      next()
-    }
-  }
-}
+const Semaphore = require('./semaphore')
 
 const apiSemaphore = new Semaphore(3)
 
